b38f85143bc32f85d25bc0bf240d3f8940762e3e
Loki17 <Tkl33pac@proton.me>
2024-05-27 11:45:24 +0100

    first dom

---
+++ DomExercises/first-series/Exercise-1/app.js
@@ -0,0 +1,25 @@
+const bold = document.querySelector("b");
+bold.parentElement.parentElement.parentElement.parentElement.style.backgroundColor =
+  "beige";
+bold.style.color = "red";
+bold.parentElement.parentElement.style.cssText =
+  "background-color: darkblue; color: white;";
+bold.parentElement.nextElementSibling.style.color = "pink";
+
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstElementChild.lastElementChild.style.color =
+  "orange";
+
+Array.from(
+  bold.parentElement.parentElement.parentElement.parentElement.firstElementChild
+    .firstElementChild.lastElementChild.children
+).forEach((element) => {
+  element.firstElementChild.style.color = "green";
+});
+
+console.log(
+  bold.parentElement.parentElement.parentElement.parentElement.firstElementChild
+    .firstElementChild.firstElementChild
+);
+
+bold.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.firstElementChild.style.cssText =
+  "text-transform: uppercase";

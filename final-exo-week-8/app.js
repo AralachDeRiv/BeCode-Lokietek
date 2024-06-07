2d9d8f606b33c2757f271261cdc7deb39bace50c
Loki17 <Tkl33pac@proton.me>
2024-06-07 15:43:23 +0100

    start final-exo-week8

---
+++ final-exo-week-8/app.js
@@ -0,0 +1,16 @@
+/*Header*/
+
+const hamburger = document.querySelector(".hamburger");
+const cross = document.querySelector(".cross");
+const mobileMenu = document.querySelector(".menu-small-format");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.style.right = 0;
+});
+cross.addEventListener("click", () => {
+  mobileMenu.style.right = "-100%";
+});
+
+/*Main*/
+
+AOS.init();

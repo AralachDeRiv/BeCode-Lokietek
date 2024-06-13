9b3901fbbdfe0cc50af84c3c585899b663dde02d
Loki17 <Tkl33pac@proton.me>
2024-06-13 15:16:14 +0100

    caroussel

--- final-exo-week-8/app.js
+++ final-exo-week-8/app.js
@@ -15,3 +15,77 @@ cross.addEventListener("click", () => {
 
 AOS.init();
 window.addEventListener("load", AOS.refresh);
+
+// Modal Setting
+
+const btnContact1 = document.querySelector(".contact1");
+const dialogContact = document.querySelector(".dialog-contact");
+const form1 = document.querySelector(".dialog-contact form");
+const errorMsg = document.querySelector(".error-msg");
+
+btnContact1.addEventListener("click", () => {
+  dialogContact.showModal();
+  form1.style.opacity = 1;
+});
+
+dialogContact.addEventListener("click", (e) => {
+  const rect = dialogContact.getBoundingClientRect();
+  if (
+    e.clientX < rect.left ||
+    e.clientX > rect.right ||
+    e.clientY < rect.top ||
+    e.clientY > rect.bottom
+  ) {
+    form1.style.opacity = 0;
+    setTimeout(() => {
+      dialogContact.close();
+    }, 650);
+  }
+});
+
+dialogContact.addEventListener("submit", (e) => {
+  e.preventDefault();
+  errorMsg.innerHTML = "";
+  let name = document.getElementById("name").value;
+  let company = document.getElementById("company").value;
+  let email = document.getElementById("email").value;
+  let emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  if (!name || !company || !email) {
+    errorMsg.innerHTML = "<p>All gaps have to be filled!";
+  } else if (!email.match(emailPattern)) {
+    errorMsg.innerHTML += "<p>Email not valid!";
+  } else {
+    console.log([name, company, email]);
+    form1.style.opacity = 0;
+    setTimeout(() => {
+      dialogContact.close();
+    }, 650);
+  }
+});
+
+// Swiper logo clients
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Configuration Swiper
+//   var swiper = new Swiper(".mySwiperLogo", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+
+//     autoplay: {
+//       delay: 3500,
+//       disableOnInteraction: false,
+//     },
+//     loop: true,
+//   });
+// });
+var swiper = new Swiper(".mySwiperLogo", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
